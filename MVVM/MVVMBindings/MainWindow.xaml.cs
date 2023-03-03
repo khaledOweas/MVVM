9679f0d90f81e91e32ea4a3ec8a288c68f032b23
Khaled Oweas <khaled.oweas@yahoo.com>
2023-03-03 08:57:12 +0200

    Init Project & create view model with Property Changed Handler

---
+++ MVVM/MVVMBindings/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MVVMBindings.ViewModels;
+using System.Windows;
+
+namespace MVVMBindings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+            var viewModel = new ViewModel();
+            viewModel.FirstName = " Khaled ";
+            DataContext = viewModel;
+
+            InitializeComponent();
+
+            viewModel.FirstName = " Changed Name ";
+            viewModel.OnPropertyChanged(nameof(ViewModel.FirstName));
+
+        }
+    }
+}
---
+++ MVVM/MVVMBindings/ViewModel/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel;
+
+namespace MVVMBindings.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string FirstName { get; set; }
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
